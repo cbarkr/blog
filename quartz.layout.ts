8716219ef5c4114ff9222590d354be31f9fe70d3
cbarkr <cbarkr@gmail.com>
2024-05-27 23:53:38 -0700

    add `Explorer` component to default layout on mobile

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -26,6 +26,7 @@ export const defaultContentPageLayout: PageLayout = {
   right: [
     // Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
+    Component.MobileOnly(Component.Explorer()),
     Component.Backlinks(),
   ],
 }
