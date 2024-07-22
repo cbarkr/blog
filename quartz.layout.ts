cdcaa38d845e51ba201eb325bc3e76b45f457906
cbarkr <cbarkr@gmail.com>
2024-07-22 13:14:57 -0700

    update desktop layout

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -23,7 +23,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Search(),
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
-    Component.DesktopOnly(Component.RecentNotes({ linkToMore: "/tags" as SimpleSlug })),
+    // Component.DesktopOnly(Component.RecentNotes({ linkToMore: "/tags" as SimpleSlug })),
   ],
   right: [
     Component.Graph(),
